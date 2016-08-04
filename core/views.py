bf515dfc582768c533f699a65c963d3c502a50fa
aswinkp <kp.aswinkumar@gmail.com>
2016-08-04 14:17:07 +0530

    Added core app and webpack

---
+++ core/views.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.shortcuts import render
+from django.views.generic import View, TemplateView
+
+# Create your views here.
+
+class AngularApp(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AngularApp, self).get_context_data(**kwargs)
+        context['ANGULAR_URL'] = settings.ANGULAR_URL
+        return context
+
+
+class SampleView(View):
+	"""View to render django template to angular"""
+	def get(self, request):
+		return render("OK!")
+		
